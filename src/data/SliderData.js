449623a74f2d66c2cdf2f9597e4288e10ca65918
Luke O <38858962+lwoluke@users.noreply.github.com>
2022-03-26 14:00:40 -0400

    Add files via upload

---
+++ src/data/SliderData.js
@@ -0,0 +1,76 @@
+import ImageOne from '../images/one.jpg';
+import ImageTwo from '../images/two.jpg';
+import ImageThree from '../images/three.jpg';
+import ImageFour from '../images/four.jpg';
+
+export const SliderData = [
+    {
+        title: 'Lake House in Westport, NY',
+        price: '$937,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'Bungalow in Lake Placid, NY',
+        price: '$520,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House'
+    },
+    {
+        title: 'Estate in Lake George, NY',
+        price: '$4,198,100',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House'
+    },
+    {
+        title: 'Mansion in Saratoga Springs, NY',
+        price: '$2,954,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House'
+    }
+
+];
+
+// export const SliderDataTwo = [
+//     {
+//         title: 'Mansion in Saratoga Springs, NY',
+//         price: '2,954,000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageFive,
+//         alt: 'House'
+//     },
+//     {
+//         title: 'Lake House in Westport, NY',
+//         price: '937,000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageSix,
+//         alt: 'House'
+//     },
+//     {
+//         title: 'Estate in Lake George, NY',
+//         price: '4,198,100',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageSeven,
+//         alt: 'House'
+//     },
+//     {
+//         title: 'Lake House in Saranac Lake, NY',
+//         price: '2,750,000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageEight,
+//         alt: 'House'
+//     }
+
+// ];
