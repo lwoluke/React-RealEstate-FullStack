39bdf52897377f425fd00c22f5ac2f1653b7f5a7
Luke O <38858962+lwoluke@users.noreply.github.com>
2022-03-26 16:23:02 -0400

    Delete src directory

--- src/data/MenuData.js
+++
@@ -1,6 +0,0 @@
-export const menuData = [
-    // { title: 'Search', link: 'search'},
-    { title: 'About', link: 'about'},
-    { title: 'Homes', link: 'homes'},
-    { title: 'Rentals', link: 'rentals'}
-]
