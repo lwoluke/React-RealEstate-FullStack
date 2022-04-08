7cd6c2b8fc8e5ae8b398be7af7cec0ad48798532
Luke O <38858962+lwoluke@users.noreply.github.com>
2022-04-07 22:07:28 -0400

    Add files via upload

---
+++ Front-End/src/components/Pages/Home/Home.jsx
@@ -0,0 +1,24 @@
+import Section1 from "./Sections/Section1";
+import Section2 from "./Sections/Section2";
+import Section3 from "./Sections/Section3";
+import Section4 from "./Sections/Section4";
+import Section5 from "./Sections/Section5";
+import Footer from "./Sections/Footer";
+import Hero from "components/features/Hero";
+import { SliderData } from 'data/SliderData';
+
+const Home = () => {
+  return (
+    <main>
+      <hero><Hero slides={ SliderData }/></hero>
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+      <Footer />
+    </main>
+  );
+};
+
+export default Home;
